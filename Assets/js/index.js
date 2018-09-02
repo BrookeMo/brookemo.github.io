2fc6753ae644526ed740a8429e21d948c9d75169
Brooke Mortensen <brookeamymo@gmail.com>
2018-09-01 19:53:03 -0600

    fixed animation reruns and updated layout

---
+++ Assets/js/index.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    setTimeout(function(){
+
+    $("#name").removeClass("tracking-in-expand");
+    $("#profilepic").removeClass("scale-in-center");
+    $("#tagline").removeClass("swing-in-top-fwd");
+    $(".link").removeClass("rotate-in-center");
+
+    }, 1000);
+});
